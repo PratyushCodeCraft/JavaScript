49421c63e867fbf6e6f248de95c592dada47b281
Pratyush <me.kumarpratyush@gmail.com>
2024-11-19 21:52:05 +0530

    Understanding Lexical Environment

---
+++ LexicalEnvironment.js
@@ -0,0 +1,21 @@
+function hello() {
+    let val = 100;
+    console.log(val)
+    {
+        let a = 10;
+        console.log(a)
+        console.log(val)
+
+    }
+        {
+            let b = 20;
+            console.log(b)
+            console.log(a);
+            console.log(val)
+    
+        }
+    
+}
+
+
+hello()

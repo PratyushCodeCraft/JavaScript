e6d622507edab407d84ae4dea675f6d6c4149583
Pratyush <me.kumarpratyush@gmail.com>
2024-11-21 21:39:00 +0530

    Understand about conditional operator

---
+++ ConditionalOperator.js
@@ -0,0 +1,9 @@
+// syntax 
+// (condition) ? if condition true -> res1 : if condition false -> res2 
+
+let age = 35;
+
+var checkAge = (age > 18) ? "Eligible for vote" : "Not Eligible for vote"
+
+console.log(checkAge);
+

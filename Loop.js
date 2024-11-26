dbf04763c89b4fb7f2d200a625e2c70a0cace109
Pratyush <me.kumarpratyush@gmail.com>
2024-11-26 22:31:52 +0530

    understand about for loop

---
+++ Loop.js
@@ -0,0 +1,13 @@
+// syntax
+// for(initialization; condition; increment/decrement)
+// example: for(int i = 0; i < 5; i++)
+
+// console.log("hello sir1")
+// console.log("hello sir 2")
+
+for(let i = 1; i <= 10; i++){
+    console.log("hello sir", i)
+}
+
+
+

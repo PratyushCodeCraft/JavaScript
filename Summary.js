560db244cb118f1ba09d8984727cbc2eb2964d3d
Pratyush <me.kumarpratyush@gmail.com>
2024-12-03 20:54:08 +0530

    summy of JS

---
+++ Summary.js
@@ -0,0 +1,118 @@
+const accountId = 12345;
+let accountEmail = "rajpratyush111@gmail.com";
+var accountPassword ="hazaribag";
+let accountStatus;
+
+//var accountPassword ="gurgaon"; <-- var can be reassign new value -->
+//let accountEmail = "pratyush111@gmail.com";
+//const accountId = 123456;
+
+console.log(accountId,accountEmail,accountPassword,accountStatus);
+
+// VIDEO 5 <-- DataType and ECMAscript standards -->
+ 
+ 
+let name ="pratyush"
+let age = 24
+let isLoggedInn = false
+let state;
+
+// Number =>
+// string => ""
+// boolean => true/false
+// null => standaLone value
+//undefined => value not assign only variable can declared
+// symbol => unique
+console.log(typeof age);
+console.log(typeof null);
+
+
+//VIDEO 6 <-- Datatype Conversion -->
+ 
+let score = 33
+let score2 ="33abc"    //"33"
+
+console.log(typeof(score));
+console.log(typeof(score2))
+
+// convert string into number =>
+    let newValue=Number(score2);
+    console.log(typeof(newValue));
+    console.log(newValue)
+    
+   // "33" => 33
+   // "33abc" => NaN
+   // true => 1 ; false => 0
+
+    let isLoggedIn = 1 
+
+    let booleanIsLoggedIn = Boolean(isLoggedIn)
+    console.log(booleanIsLoggedIn)
+    console.log(typeof booleanIsLoggedIn)
+
+    // 1 => true; 0 => false 
+    // "" => false
+    // "hitesh" => true
+
+    let someNumber =33
+
+    let stringNumber = String(someNumber);
+    console.log(stringNumber);
+    console.log(typeof (stringNumber));
+    
+
+
+// VIDEO 7 
+
+// *******************Operation*****************
+
+let value =3;
+let negValue = -value;
+console.log(negValue); // -3
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+"");  // 0
+
+
+//VIDEO 8 <-- Comparison of DataType -->
+
+//   console.log(2 > 1);
+//   console.log(2 >= 1);
+//   console.log(2 < 1);
+//   console.log(2 == 1);
+//   console.log(2 != 1);
+
+   console.log(null > 0); // false
+   console.log(null == 0); // false
+   console.log(null >= 0); // true
+
+   console.log(undefined == 0); // false
+   console.log(undefined > 0); // false
+   console.log(undefined < 0); // false
+   
+   // (===) strict check <-- means also check data type -->
+
+   console.log("2" == 2); // true
+   console.log("2" === 2); // false => because of strict check operator
+   
+
+  // VIDEO 9 <-- Data types of Javascript summary -->
+
+    // (a) primitive
+
+    // 7 types : String, Number, Boolean, Undefined, Symbol, BigInt
+
+    // (b) Non primitive (Reference)
+
+    // Array, Object, Functions
+
+
+
+   
+  
+
+
+
+
+

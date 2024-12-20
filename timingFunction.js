d71a76693718e9d9f31b3185933de8a9138997c5
Pratyush <me.kumarpratyush@gmail.com>
2024-12-20 23:59:58 +0530

    timing

--- timingFunction.js
+++ timingFunction.js
@@ -1,9 +1,44 @@
 
-setTimeout(hello,10000); // here hello is callback function bcoz hello pass as an argument 
+// setTimeout(hello,10000); // here hello is callback function bcoz hello pass as an argument 
 
-function hello(){
-    console.log("hello happy")
-}
-// call bACK FUNCTION
+// function hello(){
+//     console.log("hello happy")
+// }
 
-//call back 
+// doubt
+
+
+/////////////////////////////////////////////////////
+// function fun1(){
+//     return 2;
+// }
+// function fun2(){
+//     return 4;
+// }
+// let a = (fun1(),fun2());
+// console.log(a)
+/////////////////////////////////////////////////////
+// includes()
+//////////////////////////////////////////////////////
+// const arr =["one","two","three"];
+// const str ='hello';
+
+// const res=arr.includes('on');// false
+// const anotherVar =arr.includes('onetwo');//false
+// const newVariable=str.includes('h');//true
+
+// console.log(newVariable);
+//////////////////////////////////////////////////////
+// console.log(true =='');//false 
+// console.log('1'==1);//true
+// console.log('1'===1);//false
+///////////////////////////////////////////////////////
+// let a=10;
+// let b = new Number(10);
+// let c =10;
+
+// console.log(a === b); //false
+/////////////////////////////////////////////////////
+// What i shallow copy and deep copy ?
+/////////////////////////////////////////////////////
+

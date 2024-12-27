5afc795ffad04e0092af205efbadfcd8b876d014
Pratyush <me.kumarpratyush@gmail.com>
2024-12-27 18:16:11 +0530

    timingFunction

--- timingFunction.js
+++ timingFunction.js
@@ -1,72 +1,16 @@
 
-// setTimeout(hello,10000); // here hello is callback function bcoz hello pass as an argument 
+setTimeout(hello,10000); // here hello is callback function bcoz hello pass as an argument 
 
-// function hello(){
-//     console.log("hello happy")
-// }
-
-// doubt
-
-
-/////////////////////////////////////////////////////
-// function fun1(){
-//     return 2;
-// }
-// function fun2(){
-//     return 4;
-// }
-// let a = (fun1(),fun2());
-// console.log(a)
-/////////////////////////////////////////////////////
-// includes()
-//////////////////////////////////////////////////////
-// const arr =["one","two","three"];
-// const str ='hello';
-
-// const res=arr.includes('on');// false
-// const anotherVar =arr.includes('onetwo');//false
-// const newVariable=str.includes('h');//true
-
-// console.log(newVariable);
-//////////////////////////////////////////////////////
-// console.log(true =='');//false 
-// console.log('1'==1);//true
-// console.log('1'===1);//false
-///////////////////////////////////////////////////////
-// let a=10;
-// let b = new Number(10);
-// let c =10;
-
-// console.log(a === b); //false
-//////////////////////////////////////////////////////
-// What i shallow copy and deep copy ?
-//////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
-
-
-
-function callback(){
-    console.log('happy is calling callback function')
+function hello(){
+    console.log("hello happy")
 }
 
-const add = function(a, b, callback){
-    var result = a+b;
-    console.log(result);
-    callback();
-}
-//add(3,4,callback);
-add( 5 , 5 , () => console.log('add completed')); 
-function callback(){
-    console.log('happy is calling callback function')
-}
 
-const add = function(a, b, callback){
-    var result = a+b;
-    console.log(result);
-    callback();
-}
-//add(3,4,callback);
-add( 5 , 5 , () => console.log('add completed')); 
+
+
+
+
+
 
 
 

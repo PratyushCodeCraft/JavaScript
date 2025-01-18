efa8d40a609cbd30ba5021c5f6d9052fd9b8013d
Pratyush <me.kumarpratyush@gmail.com>
2025-01-18 22:03:04 +0530

    timingFunction

--- timingFunction.js
+++ timingFunction.js
@@ -5,6 +5,12 @@ function hello(){
     console.log("hello happy")
 }
 
+setTimeout(hello,10000); // here hello is callback function bcoz hello pass as an argument 
+
+function hello(){
+    console.log("hello happy")
+}
+
 
 
 
